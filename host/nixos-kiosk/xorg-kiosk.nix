a2864b31785bf50c1ca737eb7c25fa3abe6197fd
vasuper <vasu3797@gmail.com>
2023-06-25 20:46:38 -0500

    fixing xorg-kiosk.nix

--- host/nixos-kiosk/xorg-kiosk.nix
+++ host/nixos-kiosk/xorg-kiosk.nix
@@ -1,128 +1,62 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running `nixos-help`).
+{ pkgs, ... }:
 
-{ config, pkgs, ... }:
+let
+  kioskUsername = "kiosk";
+  browser = pkgs.firefox;
+  autostart = ''
+    #!${pkgs.bash}/bin/bash
+    # End all lines with '&' to not halt startup script execution
 
-{
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./xorg-kiosk.nix
-    ];
+    # https://developer.mozilla.org/en-US/docs/Mozilla/Command_Line_Options
+    firefox --kiosk https://xkcd.com/ &
+  '';
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  virtualisation.docker.enable = true;
-  virtualisation.docker.rootless = {
-  	enable = true;
-	setSocketVariable = true;
+  inherit (pkgs) writeScript;
+in {
+  # Set up kiosk user
+  users.users = {
+    "${kioskUsername}" = {
+      group = kioskUsername;
+      isNormalUser = true;
+      packages = [ browser ];
+    };
   };
-  networking.hostName = "nixos"; # Define your hostname.
-  # Pick only one of the below networking options.
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  users.groups."${kioskUsername}" = {};
 
-  # Set your time zone.
-  time.timeZone = "America/Chicago";
+  # Configure X11
+  services.xserver = {
+    enable = true;
+    layout = "us"; # keyboard layout
+    libinput.enable = true;
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+    # Let lightdm handle autologin
+    displayManager.lightdm = {
+      enable = true;
+      autoLogin = {
+        enable = true;
+        timeout = 0;
+        user = kioskUsername;
+      };
+    };
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkbOptions in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.mechteam = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "docker" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [
-      firefox
-      tree
-    ];
+    # Start openbox after autologin
+    windowManager.openbox.enable = true;
+    displayManager.defaultSession = "none+openbox";
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-        neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by def>
-        wget
-        clevis
-        cryptsetup
-        curl
-        jq
-        git
-        tldr
-        netplan
-        procps
-        qemu-utils
-	firefox
-        rsyslog
-        unzip
-        wget
-	gh
-  	python311
-  	docker
-	openbox
+  # Overlay to set custom autostart script for openbox
+  nixpkgs.overlays = with pkgs; [
+    (self: super: {
+      openbox = super.openbox.overrideAttrs (oldAttrs: rec {
+        postFixup = ''
+          ln -sf /etc/openbox/autostart $out/etc/xdg/openbox/autostart
+        '';
+      });
+    })
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
-
+  # By defining the script source outside of the overlay, we don't have to
+  # rebuild the package every time we change the startup script.
+  environment.etc."openbox/autostart".source = writeScript "autostart" autostart;
 }
 
